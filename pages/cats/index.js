c708ab85cb7693acd6efbd78aebadca581c0e1d4
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-06-28 23:03:15 -0400

    completed Fetching Data section

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,11 +1,42 @@
-import React from 'react'
-import Nav from '../../components/Nav/Nav'
+import React, { useEffect, useState } from 'react';
+import Card from '../../components/Card/Card';
+import Nav from '../../components/Nav/Nav';
 
 export default function index() {
-  return (
-    <div>
-      <Nav />
-      
-    </div>
-  )
+	const [cats, setCats] = useState([]);
+
+	const fetchCats = async () => {
+		const response = await fetch('/api/cats');
+		const data = await response.json();
+		console.log(data);
+		setCats(data);
+	};
+
+	useEffect(() => {
+		fetchCats();
+	}, []);
+
+	return (
+		<>
+			<Nav />
+			<div className="container mt-5">
+				<div
+					style={{
+						display: "flex",
+						flexWrap: "wrap"
+					}}
+				>
+					{cats.map((cat) => (
+						<Card
+							name={cat.name}
+							id={cat.id}
+							phone={cat.phone}
+							email={cat.email}
+							image={cat.image}
+						/>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
